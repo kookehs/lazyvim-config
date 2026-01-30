e38e574edd8fec15a4887ddc6ef064a10f67bd22
Bill Lin <4277184+kookehs@users.noreply.github.com>
2026-01-29 23:24:16 -0800

    fix: correct accept behavior for code completion

--- lua/plugins/llama.lua
+++ lua/plugins/llama.lua
@@ -5,22 +5,18 @@ return {
       endpoint_fim = "http://127.0.0.1:8012/infill",
       endpoint_inst = "http://127.0.0.1:8012/v1/chat/completions",
       keymap_debug_toggle = "",
+      keymap_fim_accept_full = "<Tab>",
+      keymap_fim_accept_line = "<S-Tab>",
+      keymap_fim_accept_word = "",
       keymap_fim_trigger = "",
+      keymap_inst_accept = "<Tab>",
+      keymap_inst_canel = "<Esc>",
       keymap_inst_continue = "",
       keymap_inst_rerun = "",
-      keymap_inst_trigger = "",
+      max_line_suffix = 1,
       model_fim = "google/codegemma-1.1-2b-GGUF",
       model_inst = "ggml-org/gemma-3-4b-it-qat-GGUF",
+      show_info = 0,
     }
-  end,
-  config = function()
-    require("which-key").add({
-      {
-        mode = { "v" },
-        nowait = true,
-        remap = false,
-        { "<leader>lli", "<cmd>LlamaInstruct<cr>", desc = "Llama Instruct" },
-      },
-    })
   end,
 }
